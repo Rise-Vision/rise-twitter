5f6b4b554bb4879867f570d090a706d7dca06333
rodrigopavezi <rodrigopavezi@gmail.com>
2017-12-12 18:32:02 -0200

    Skip manifest changes

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,11 +7,12 @@ echo "deploying $VERSION"
 git clone git@github.com:Rise-Vision/private-keys.git
 gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
 
-mkdir -p manifests
-gsutil cp gs://install-versions.risevision.com/display-modules-*.json manifests
+# Commented out manifest changes for now
+#mkdir -p manifests
+#gsutil cp gs://install-versions.risevision.com/display-modules-*.json manifests
 
-find manifests -name "*.json" -exec node ./node_modules/common-display-module/update-module-version.js '{}' $COMPONENT_NAME $VERSION 0 "component" \;
-gsutil cp manifests/*.json gs://install-versions.risevision.com/staging/components/$COMPONENT_NAME/$VERSION
+#find manifests -name "*.json" -exec node ./node_modules/common-display-module/update-module-version.js '{}' $COMPONENT_NAME $VERSION 0 "component" \;
+#gsutil cp manifests/*.json gs://install-versions.risevision.com/staging/components/$COMPONENT_NAME/$VERSION
 gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/staging/components/$COMPONENT_NAME/$VERSION/*
 gsutil setmeta -h "Content-Disposition:attachment" gs://install-versions.risevision.com/staging/components/$COMPONENT_NAME/$VERSION/*.sh
 gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/staging/components/$COMPONENT_NAME/$VERSION/*
