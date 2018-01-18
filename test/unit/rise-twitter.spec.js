dbbf59f735c4902899488e79f4e0f6864ea18824
rodrigopavezi <rodrigopavezi@gmail.com>
2018-01-18 20:00:15 -0200

    Implement listener for configure event

--- test/unit/rise-twitter.spec.js
+++ test/unit/rise-twitter.spec.js
@@ -1,10 +1,33 @@
 import RiseTwitter from "../../src/rise-twitter";
 
-describe("Example test", () => {
-  it("works", () => {
+describe("Twitter Component - Unit", () => {
+  it("should have component defined", () => {
     expect(RiseTwitter).toBeDefined();
 
     const component = new RiseTwitter();
     expect(component).toBeDefined();
+  });
+
+  it("should call configure listener when configure event is dispached", (done) => {
+    jest.useFakeTimers();
+
+    const risePlaylistItem = document.createElement("rise-playlist-item");
+    document.getElementsByTagName("body")[0].appendChild(risePlaylistItem);
+    const component = new RiseTwitter();
+
+    const event = new CustomEvent("configure", {
+      detail: {
+        screenName: "test"
+      }
+    });
+
+    risePlaylistItem.dispatchEvent(event);
+
+    setTimeout(()=>{
+      expect(component.screenName).toEqual("test");
+      done();
+    }, 1000);
+
+    jest.runAllTimers();
   });
 });
