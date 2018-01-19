67f8f1841562bf38e173fa9684baae12b255b1eb
rodrigopavezi <rodrigopavezi@gmail.com>
2018-01-19 12:30:17 -0200

    Finish unit tests

--- test/unit/rise-twitter.spec.js
+++ test/unit/rise-twitter.spec.js
@@ -1,19 +1,25 @@
 import RiseTwitter from "../../src/rise-twitter";
 
+let component = null;
+let risePlaylistItem = null;
 describe("Twitter Component - Unit", () => {
+  beforeAll(() => {
+    risePlaylistItem = document.createElement("rise-playlist-item");
+    document.getElementsByTagName("body")[0].appendChild(risePlaylistItem);
+    component = new RiseTwitter();
+
+    component._play = jest.genMockFn();
+    component._pause = jest.genMockFn();
+    component._stop = jest.genMockFn();
+  });
+
   it("should have component defined", () => {
     expect(RiseTwitter).toBeDefined();
-
-    const component = new RiseTwitter();
     expect(component).toBeDefined();
   });
 
   it("should call configure listener when configure event is dispached", (done) => {
     jest.useFakeTimers();
-
-    const risePlaylistItem = document.createElement("rise-playlist-item");
-    document.getElementsByTagName("body")[0].appendChild(risePlaylistItem);
-    const component = new RiseTwitter();
 
     const event = new CustomEvent("configure", {
       detail: {
@@ -25,6 +31,51 @@ describe("Twitter Component - Unit", () => {
 
     setTimeout(()=>{
       expect(component.screenName).toEqual("test");
+      done();
+    }, 1000);
+
+    jest.runAllTimers();
+  });
+
+  it("should call play listener when play event is dispached", (done) => {
+    jest.useFakeTimers();
+
+    const event = new CustomEvent("play");
+
+    risePlaylistItem.dispatchEvent(event);
+
+    setTimeout(()=>{
+      expect(component._play).toHaveBeenCalled();
+      done();
+    }, 1000);
+
+    jest.runAllTimers();
+  });
+
+  it("should call pause listener when pause event is dispached", (done) => {
+    jest.useFakeTimers();
+
+    const event = new CustomEvent("play");
+
+    risePlaylistItem.dispatchEvent(event);
+
+    setTimeout(()=>{
+      expect(component._play).toHaveBeenCalled();
+      done();
+    }, 1000);
+
+    jest.runAllTimers();
+  });
+
+  it("should call stop listener when stop event is dispached", (done) => {
+    jest.useFakeTimers();
+
+    const event = new CustomEvent("play");
+
+    risePlaylistItem.dispatchEvent(event);
+
+    setTimeout(()=>{
+      expect(component._play).toHaveBeenCalled();
       done();
     }, 1000);
 
