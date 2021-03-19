fe62d3a673f582c9d82d08668a3527c97e3022c9
Stuart Lees <stuartlees@gmail.com>
2021-03-19 11:31:38 -0400

    Update error codes

--- src/rise-twitter.js
+++ src/rise-twitter.js
@@ -129,7 +129,7 @@ export default class RiseTwitter extends HTMLElement {
 
   _validateConfiguration() {
     if (!this.id) {
-      this.logger.error('Error: componnentId is missing', null, 'E000000089');
+      this.logger.error('Error: componnentId is missing', null, 'E000000086');
       this.eventHandler.emitDone();
     }
 
@@ -192,7 +192,7 @@ export default class RiseTwitter extends HTMLElement {
 
       this._startWaitingForTweetsTimer();
     } else {
-      this.logger.error('Error: component is not connected to required modules', null, 'E000000025');
+      this.logger.error('Error: component is not connected to required modules', null, 'E000000091');
       this.tweet.handleError();
     }
   }
